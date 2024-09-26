ca9768e723ebeb32f9270a3579b1d1046a11d6f2
Yaroslav Kuzenko <yaroslavkuzenko02@gmail.com>
2024-09-26 23:59:50 +0300

    change models

--- backend/models/models.py
+++ backend/models/models.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 
-from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON
+from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON, Boolean
 
-metadata: MetaData = MetaData()
+metadata = MetaData()
 
 role = Table(
     "role",
@@ -17,7 +17,10 @@ user = Table(
     metadata,
     Column("id", Integer, primary_key=True),
     Column("email", String, nullable=False),
-    Column("password", String, nullable=False),
     Column("registered_at", TIMESTAMP, default=datetime.utcnow),
     Column("role_id", Integer, ForeignKey(role.c.id)),
+    Column("hashed_password", String, nullable=False),
+    Column("is_active", Boolean, default=True, nullable=False),
+    Column("is_superuser", Boolean, default=False, nullable=False),
+    Column("is_verified", Boolean, default=False, nullable=False),
 )
