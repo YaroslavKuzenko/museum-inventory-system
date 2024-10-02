94bb0effb2f363305b1cd342e869ae8408bdc439
Yaroslav Kuzenko <yaroslavkuzenko02@gmail.com>
2024-10-03 00:58:31 +0300

    change auth.service.ts

--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,35 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   register(userData: any) {
-    return this.http.post(`${this.apiUrl}/register`, userData);
+    const registerModel = {
+      email: userData.email,
+      password: userData.password,
+      is_active: true,
+      is_superuser: false,
+      is_verified: false,
+      role_id: 1
+    }
+    return this.http.post(`${this.apiUrl}/auth/register`, registerModel);
   }
 
-  login(credentials: any) {
-    return this.http.post(`${this.apiUrl}/login`, credentials);
+  login(email: string, password: string): Observable<any> {
+    const body = new HttpParams()
+      .set('grant_type', 'password')
+      .set('username', email)
+      .set('password', password)
+      .set('scope', '')
+      .set('client_id', '')
+      .set('client_secret', '');
+
+    return this.http.post(`${this.apiUrl}/auth/login`, body.toString(), {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      withCredentials: true,
+    });
   }
+
+  // don't work
+  isLoggedIn(): boolean {
+    return document.cookie.includes('mis');
+  }
+
 }
